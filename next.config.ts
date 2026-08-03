611e70cc9a489de665527493013a557c803ebf3e
YaodeChiang4181 <YaodeChiang4181@users.noreply.github.com>
2026-08-03 16:46:06 +0800

    fix(config): update R2 domain to use public dev URL

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'd918009a978a5f83b6d3c98120c2482d.r2.cloudflarestorage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-c092bc5012f54e4f90b8490f26cefdd4.r2.dev',
       }
     ],
   },
